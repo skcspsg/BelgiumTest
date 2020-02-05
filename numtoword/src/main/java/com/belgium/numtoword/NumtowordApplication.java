e1dbb430ff88c5e410caf6a8b810d9f785d5d022
skcspsg <60703204+skcspsg@users.noreply.github.com>
2020-02-05 20:36:30 +0530

    Basic spring boot app with TDD context test

---
+++ numtoword/src/main/java/com/belgium/numtoword/NumtowordApplication.java
@@ -0,0 +1,13 @@
+package com.belgium.numtoword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumtowordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumtowordApplication.class, args);
+	}
+
+}
